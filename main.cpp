1e89d42da6608ecb57450cc464e369db14d9c2c5
Jedi Lee <jedilee10@gmail.com>
2025-05-24 22:27:03 +0900

    chore: initial scaffold for project structure, add student_data array

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  C867 PRFA — JYM2 Task 1: Class Roster
+//
+// Created by Jedi Lee 5/24/2025
+
+#include <iostream>
+#include <string>
+using namespace std;
+#include "roster.h"
+
+int main() {
+    // B. Modify the attached "studentData Table" to include your personal info as the last entry in this array.
+    const string studentData[] = {
+        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        "A5,Jedi,Lee,jlee1146@wgu.edu,34,7,2,14,SOFTWARE"
+    };
+
+	return 0;
+}
